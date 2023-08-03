635a09e81420586a7f18efe1c93b1f3e4e686b03
Kimleng <kimlenghor2006@gmail.com>
2023-08-03 17:15:43 -0500

    Initial Commit

---
+++ BuddyUP/BuddyUPApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuddyUPApp.swift
+//  BuddyUP
+//
+//  Created by Kimleng Hor on 8/3/23.
+//
+
+import SwiftUI
+
+@main
+struct BuddyUPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
